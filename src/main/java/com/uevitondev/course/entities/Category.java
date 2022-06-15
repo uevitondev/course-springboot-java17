c26b367b229b3ad6cab0c3fd16cf6efa4ffd1955
uevitondev <uevitonsantos@outlook.com>
2022-06-14 21:24:07 -0300

    Product

--- src/main/java/com/uevitondev/course/entities/Category.java
+++ src/main/java/com/uevitondev/course/entities/Category.java
@@ -1,13 +1,16 @@
 package com.uevitondev.course.entities;
 
 import java.io.Serializable;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.persistence.Transient;
 
 @Entity
 @Table(name = "tb_category")
@@ -20,6 +23,9 @@ public class Category implements Serializable {
 	private Long id;
 
 	private String name;
+	
+	@Transient
+	private Set<Product> products = new HashSet<Product>();
 
 	public Category() {
 
@@ -47,8 +53,8 @@ public class Category implements Serializable {
 		this.name = name;
 	}
 
-	public static long getSerialversionuid() {
-		return serialVersionUID;
+	public Set<Product> getProducts() {
+		return products;
 	}
 
 	@Override
---
+++ src/main/java/com/uevitondev/course/repositories/ProductRepository.java
@@ -0,0 +1,10 @@
+
+package com.uevitondev.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.uevitondev.course.entities.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+}
