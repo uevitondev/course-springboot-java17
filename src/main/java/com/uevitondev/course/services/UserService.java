5b2b0a2017a3d8773c4d26d945e53ea63b80990d
uevitondev <uevitonsantos@outlook.com>
2022-06-27 15:31:55 -0300

    Exception handling - findById

--- src/main/java/com/uevitondev/course/services/UserService.java
+++ src/main/java/com/uevitondev/course/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.uevitondev.course.entities.User;
 import com.uevitondev.course.repositories.UserRepository;
+import com.uevitondev.course.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -22,7 +23,7 @@ public class UserService {
 	public User findById(Long id) {
 
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 
 	public User insertUser(User obj) {
